18e00c3f0c627837daa0ba48aff5c404b11fed33
Peter Li <petercyli02@gmail.com>
2025-11-27 00:43:04 +0000

    changed Gemini key and downgraded main model from 2.5 pro to 2.5 flash

--- chatbot/services/llm.py
+++ chatbot/services/llm.py
@@ -36,7 +36,7 @@ class LLMService:
         )
         prompt = router_prompt.format(user_prompt=user_input)
         response = self.client.models.generate_content(
-            model="gemini-2.5-pro",
+            model="gemini-2.5-flash",
             contents=prompt,
         )
         cleaned_response_text = response.text[7:-3].strip()
