50ffdd6a4e972d43bf66b6cb0f49c8ba14c213d4
Peter Li <petercyli02@gmail.com>
2026-08-08 16:50:37 +0800

    test: corrected the deliberate error

--- chatbot/tests/test_user_service.py
+++ chatbot/tests/test_user_service.py
@@ -101,5 +101,5 @@ class UserProfileTests(TestCase):
         force_authenticate(request, user=self.user)
 
         response = ChatAPIView.as_view()(request, pk=other_conversation.id)
-        self.assertNotEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
         self.assertNotIn("secret message", str(response.data))
